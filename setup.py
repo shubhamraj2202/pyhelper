9792d48da61674980e36619c7e92a373f6b1c89e
Shubham Raj <shubhamraj2202@gmail.com>
2023-01-14 22:05:08 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyhelper-misc",
-    version="1.0.0",
+    version="1.0.1",
     description="Python Library Containing Generic Utilities and Helper Functions",
     long_description=open("README.md", encoding="utf-8").read(),
     author="Shubham Raj",
     author_email="shubhamraj2202@gmail.com",
     url="https://www.linkedin.com/in/shubhamraj2202/",
-    packages=find_packages(include=["py_helper", "py_helper.*"]),
+    packages=find_packages(include=["pyhelper", "pyhelper.*"]),
     install_requires=[],
     tests_require=["pytest"],
 )
